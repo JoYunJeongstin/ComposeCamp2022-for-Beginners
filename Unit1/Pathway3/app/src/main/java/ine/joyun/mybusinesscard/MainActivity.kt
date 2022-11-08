0abb2e35429bac4630b9af3605cb3a6f23585f21
JO_YUN_JEONG <jeongjoyun20@naver.com>
2022-11-08 17:56:02 +0900

    I have to contract these Text Composes.

--- Unit1/Pathway3/app/src/main/java/ine/joyun/mybusinesscard/MainActivity.kt
+++ Unit1/Pathway3/app/src/main/java/ine/joyun/mybusinesscard/MainActivity.kt
@@ -1,15 +1,29 @@
 package ine.joyun.mybusinesscard
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.android.style.LineHeightSpan
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ine.joyun.mybusinesscard.ui.theme.MyBusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeTheBusinessCard()
                 }
             }
         }
@@ -30,14 +44,106 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun ComposeTheBusinessCard() {
+    val TitleFontSize = 40
+    Column {
+        Text(
+            text = "Jeong Jo Yun's\nReception",
+            lineHeight = (TitleFontSize + 6).sp,
+            textAlign = TextAlign.End,
+            fontSize = TitleFontSize.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Row {
+            Column {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(
+                            top = 30.dp,
+                            start = 30.dp,
+                            end = 30.dp,
+                            bottom = 20.dp
+                        )
+                        .size(120.dp)
+                )
+                Text(
+                    text = "Profile Image",
+                    fontSize = 17.sp,
+                    modifier = Modifier.padding(start = 42.dp)
+                )
+            }
+            Column {
+                Text(
+                    "Kotlin Fundamentals",
+                    modifier = Modifier.padding(top = 30.dp),
+                    fontWeight = FontWeight.Black,
+                    fontSize = 18.sp
+                )
+                Text("  Functions, Literals.")
+                Text(
+                    "Android composable",
+                    modifier = Modifier.padding(top = 20.dp),
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 18.sp
+                )
+                Text("  Row, Column, Box, Image, Text")
+                Text(
+                    "Android composes",
+                    modifier = Modifier.padding(top = 20.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp
+                )
+                Text("  Many Things.")
+            }
+        }
+        Box(
+            modifier = Modifier
+                .padding(top = 16.0.dp)
+                .size(width = 600.dp, height = 6.dp)
+                .alpha(0.6f)
+                .background(Color.Black)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp)
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = "Kotlin", fontSize = 32.sp, fontWeight = FontWeight.W700)
+                Text(text = "Functions, Literals.")
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Android", fontSize = 32.sp, fontWeight = FontWeight.W700)
+                Text(text = "Composable")
+            }
+        }
+        Text(
+            "What's Next Project!?!?!?!??",
+            fontSize = 53.3.sp,
+            fontWeight = FontWeight.Black,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(30.dp)
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "god")
 @Composable
-fun DefaultPreview() {
+private fun PreviewTheBusinessCard() {
     MyBusinessCardTheme {
-        Greeting("Android")
+        ComposeTheBusinessCard()
     }
 }
